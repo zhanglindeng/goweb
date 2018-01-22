62edeb5e9b63f2ece53e2e18ea7787186af19458
zhanglindeng <zhanglindeng@outlook.com>
2018-01-22 22:24:45 +0800

    test

--- util/md5_test.go
+++ util/md5_test.go
@@ -3,6 +3,7 @@ package util
 import "testing"
 
 func TestMd5(t *testing.T) {
-	// e10adc3949ba59abbe56e057f20f883e
-	t.Log(Md5("123456"))
+	if Md5("123456") != "e10adc3949ba59abbe56e057f20f883e" {
+		t.Error("Md5 error")
+	}
 }
