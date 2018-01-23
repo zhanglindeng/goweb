0a68743b06ea2456504426a7eb7c21b9c73d0fac
zhanglindeng <zhanglindeng@outlook.com>
2018-01-23 22:21:31 +0800

    add test package

---
+++ test/readme.md
@@ -0,0 +1,13 @@
+# test
+
+## user
+
+### user login
+```
+go test -v -test.run TestUserLogin
+```
+
+### user register
+```
+go test -v -test.run TestUserRegister
+```
---
+++ test/user_register_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/parnurzeal/gorequest"
+)
+
+func TestUserRegister(t *testing.T) {
+
+	resp, body, errs := gorequest.New().Post("http://localhost:10086/api/user/register").
+		Set("Content-Type", "application/json").
+		Send(`{"email":"zhangsan@163.com","password":"123456"}`).
+		End()
+
+	if len(errs) > 0 {
+		t.Error(errs)
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		t.Error("status code", resp.StatusCode, resp.Status)
+		return
+	}
+
+	var data struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		Token   string `json:"token"`
+	}
+
+	if err := json.Unmarshal([]byte(body), &data); err != nil {
+		t.Error(err)
+		return
+	}
+
+	if data.Code != 0 {
+		t.Error("code", data.Code, data.Message)
+		return
+	}
+
+	t.Log(data.Token)
+}
